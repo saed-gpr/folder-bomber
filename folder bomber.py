f1ef4883c4afab4936495dd48f6c015c0c467bf3
saed gholipour <88347595+saed-gpr@users.noreply.github.com>
2022-07-26 17:07:58 +0430

    Add files via upload

---
+++ folder bomber.py
@@ -0,0 +1,14 @@
+from rand_string.rand_string import RandString
+import os
+
+
+# if you just know this tool how work, just run code line 6 and 7!!!
+for i in range(5):
+    os.mkdir('path',RandString('uppercase', 4))
+
+
+# this part of code is very danger!! do not use this. because my distroy your pc!!!
+
+
+#while True:
+    #os.mkdir('path', RandString('uppercase', 4))
